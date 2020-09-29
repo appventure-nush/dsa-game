64215b640de6f9ae1f4cce1e95d225197fd7cf78
xplus2g4 <xplus2g4@gmail.com>
2020-09-29 17:07:43 +0800

    Update description, logo

--- src/DescriptionSubpage.tsx
+++ src/DescriptionSubpage.tsx
@@ -60,7 +60,12 @@ const DescriptionSubpage: React.FC = () => {
       </Grid>
       <Grid item xs={10}>
         <Typography variant='body1'>
-          In the near future, the Worldwide Information Security and Engineering Research organisation is the world's protection against the dark threat of cyber terrorism.  Only the most elite computer scientists could dream of playing a part in safeguarding software and data all over the world. But one day, a hacker successfully breached WISER's firewalls, damaged their servers and ravaged their modules. Only you, an agent of computer science, can put a stop to the hacker's schemes and save WISER and all who depend on it.
+          The Worldwide Information Security and Engineering Research organisation protects the world against the threat of cyber terrorism. One day, a hacker damages WISER's servers, ravages their modules and puts the world in grave danger. Only you, an agent of computer science, can stop him and save WISER from evil.
+          <br/><br/>
+
+          Command your very own digital turtle with the Python programming language. 
+          Learn fundamental programming concepts such as variables, functions and iterations.
+          Wow your friends with your amazing animations and your newfound computer science powers!
         </Typography>
       </Grid>
       <Grid item xs={10}>
@@ -77,7 +82,7 @@ const DescriptionSubpage: React.FC = () => {
           </a>
           &nbsp; &nbsp;
           <a href='https://nush.app'>
-            <img height='75px' src='appventure_favicon.ico' alt='appventure logo'/>
+            <img height='75px' src='appventure_logo.png' alt='appventure logo'/>
           </a>
         </Typography>
       </Grid>
